960b3423ee1eabf6afce2da54ead8dfbdf4d4a47
Prabhath Madushanka <143389126+PrabhathShanka@users.noreply.github.com>
2024-09-23 21:00:20 +0530

    update

--- login.php
+++ login.php
@@ -1,89 +1,68 @@
 <?php
 require 'databaseConnection.php';
 
-session_start(); 
+session_start();
 
+if (isset($_POST['emailAddress']) && isset($_POST['password'])) {
 
-
-if (isset($_POST['telNumber']) && isset($_POST['password'])) {
-
-    function validate($data){
-
-       $data = trim($data);
-
-       $data = stripslashes($data);
-
-       $data = htmlspecialchars($data);
-
-       return $data;
-
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
-    $telNum = validate($_POST['telNumber']);
-
+    $telNum = validate($_POST['emailAddress']);
     $pass = validate($_POST['password']);
 
     if (empty($telNum)) {
-
-        echo "User Name is required";
-
+        echo "User email is required";
         exit();
-
-    }else if(empty($pass)){
-
-        echo"Password is required";
-
+    } else if (empty($pass)) {
+        echo "Password is required";
         exit();
-
-    }else{
-
-        $sql = "SELECT * FROM customer WHERE telephone_no='$telNum' AND password ='$pass'";
-
+    } else {
+        $sql = "SELECT * FROM users WHERE email='$telNum' AND password1='$pass'";
         $result = mysqli_query($conn, $sql);
 
         if (mysqli_num_rows($result) === 1) {
-
             $row = mysqli_fetch_assoc($result);
 
-            if ($row['telephone_no'] === $telNum && $row['password'] === $pass) {
-
-                $_SESSION['teliphon_number'] = $telNum;
-
-               header("Location: index-AfterLogin.php");
-                exit();
-            }  
-            else{
-
-              ?>
-		            <script type="text/javascript">
-                 var alertStyle = "padding: 10px; background-color: #f44336; color: white;";
-                alert("The phone number or password is invalid !!!");
-                  window.location.href = "login.html";
-                  </script>
-				    <?php
-
-                  //  header("Location: Sign in4.html");
-                  //  exit();
+            if ($row['email'] === $telNum && $row['password1'] === $pass) {
+                // Check if the user role is 'apartmentOwner' or 'students'
+                if ($row['user_role'] === 'apartmentOwner') {
+                    header("Location: index-AfterLoginApartmentOwner.php");
+                    exit();
+                } else if ($row['user_role'] === 'students') {
+                    header("Location: index-AfterLoginUser.php");
+                    exit();
+                } else {
+                    // Handle other roles or show an error
+                    ?>
+                    <script type="text/javascript">
+                    alert("You do not have permission to access this page.");
+                    window.location.href = "login.html";
+                    </script>
+                    <?php
                 }
-
-            }else{
-              ?>
-              <script type="text/javascript">
-               var alertStyle = "padding: 10px; background-color: #f44336; color: white;";
-              alert("The phone number or password is invalid !!!");
+            } else {
+                ?>
+                <script type="text/javascript">
+                alert("The email address or password is invalid !!!");
                 window.location.href = "login.html";
                 </script>
-          <?php
-  
-                   // header("Location: Sign in5.html");
-                   // exit();
-                 }
- 
-        }
-
-    }
-
-        else{
-                 exit();
+                <?php
             }
+        } else {
+            ?>
+            <script type="text/javascript">
+            alert("The email address or password is invalid !!!");
+            window.location.href = "login.html";
+            </script>
+            <?php
+        }
+    }
+} else {
+    exit();
+}
 ?>
