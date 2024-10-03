11f4b07d85dd99fbb8b7c9e5b78652a70aefa633
Dinan Dilmith <dinandilmith58@gmail.com>
2024-10-03 14:10:07 +0530

    edit

--- customerRegister.php
+++ customerRegister.php
@@ -48,7 +48,7 @@ if (isset($_POST["submitCustomer"])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Registration Form</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" type="text/css" href="index.css">
   <link rel="stylesheet" href="cusregister.css">
@@ -254,6 +254,30 @@ if (isset($_POST["submitCustomer"])) {
       </ul>
     </div>
   </nav>
+
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <img src="image/boarding_logo.jpg" alt="Your Image Description" style="
+            width: 70px;
+            height: 70px;
+            vertical-align: middle;
+            margin-left: 50px;
+        " />
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto px-5">
+                <a class="nav-item nav-link mr-4" href="index.php">Home</a>
+                <a class="nav-item nav-link mr-4" href="login.html">Sign In</a>
+                </li>
+            </div>
+        </div>
+    </nav>
+
+
+
+
   <div class="registration-container mt-5">
     <h2>Customer Registration</h2>
     <form action="#" method="POST">
